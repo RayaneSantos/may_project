fabdaa6bc5bb7b9c7e73ad5eea08ee78dd34b71a
Rayane Santos <rayanesantos@unimedce.com.br>
2020-08-18 16:54:03 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          <h2>Mayara</h2>
+            <h3>Doces e delícias</h3>
+        </div>
+        <p className="App-intro">
+          Let's have some <code>fun</code>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
